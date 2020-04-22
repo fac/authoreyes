76d63f38d79a547d62b321c3d771d008b1abe36a
Mark Pitchless <markpitchless@gmail.com>
2020-04-22 15:06:09 +0100

    Update to bundler 2 and ruby 2.6.5

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source 'https://rubygems.org'
 
 gemspec
 
-gem 'rails', '~> 5.0'
-
 group :test do
   gem 'codeclimate-test-reporter', '~> 1.0.0'
   gem 'simplecov'
--- test/factories/test_model.rb
+++ test/factories/test_model.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :test_model do
-    title Faker::GameOfThrones.character
+    title Faker::TvShows::GameOfThrones.character
     body Faker::Lorem.paragraphs
     user
   end
